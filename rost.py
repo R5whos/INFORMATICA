43dfe572df5829008e7f8d7f91c763540f4d40d0
R5whos <48851275+R5whos@users.noreply.github.com>
2021-02-17 21:45:38 +0200

    Create rost.py

---
+++ rost.py
@@ -0,0 +1,20 @@
+a = 3
+b = 4
+c = 5
+d = 6
+
+
+if a < b:
+	if b<c:
+		if c<d:
+			if d > a:
+				print('YES')
+
+			else:
+				print('NO')
+		else:
+			print('NO')
+	else:
+		print('NO')
+else:
+	print('NO')
